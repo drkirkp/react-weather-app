74f5cc37e6ce60c99a87243f64b6de797871a569
drkirkp <154014621+drkirkp@users.noreply.github.com>
2024-04-27 12:35:38 -0500

    Added Bootstrap

--- src/App.js
+++ src/App.js
@@ -4,16 +4,18 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project was coded by Dail Williams and is{" "}
-        <a
-          href="https://github.com/drkirkp/react-weather-app.git"
-          target="_blank"
-        >
-          open-sourced on GitHub
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project was coded by Dail Williams and is{" "}
+          <a
+            href="https://github.com/drkirkp/react-weather-app.git"
+            target="_blank"
+          >
+            open-sourced on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }

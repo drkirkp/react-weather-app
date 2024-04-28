548b4fb250b777e8917f1df501a56c751362447f
drkirkp <154014621+drkirkp@users.noreply.github.com>
2024-04-28 15:21:51 -0500

    Added rel="nonreferrer" per Netlify's request

--- src/App.js
+++ src/App.js
@@ -13,6 +13,7 @@ export default function App() {
           <a
             href="https://github.com/drkirkp/react-weather-app.git"
             target="_blank"
+            rel="noreferrer"
           >
             open-sourced on GitHub
           </a>
